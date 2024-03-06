2e081643e45c5d1434f481767e92e14be5a8c2c1
Solovev Evgeny <evgeny.solovev@gmail.com>
2024-03-06 12:36:12 +0300

    routes, init pages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {CalendarPage} from "./pages/CalendarPage";
+import {CreateEventPage} from "./pages/CreateEventPage";
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CalendarPage />
+  },
+  {
+    path: '/create',
+    element: <CreateEventPage />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router}>
+        <App />
+    </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
